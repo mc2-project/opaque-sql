18cc2e76baf6e7df9174639aaf9f6913c367299b
Wenting Zheng <wzheng@r1.millennium.berkeley.edu>
2016-04-03 19:09:05 -0700

    Partial work on join

--- sql/enclave/Enclave/Enclave.cpp
+++ sql/enclave/Enclave/Enclave.cpp
@@ -36,6 +36,8 @@
  *   Invokes OCALL to display the enclave buffer to the terminal.
  */
 
+#define ROW_UPPER_BOUND (2 * 1024)
+
 const char *key_str = "helloworld123123";
 const sgx_aes_gcm_128bit_key_t *key = (const sgx_aes_gcm_128bit_key_t *) key_str;
 
@@ -794,4 +796,25 @@ void ecall_oblivious_sort(int op_code, uint8_t *input, uint32_t buffer_length,
 
 	//printf("Freed data\n");
   }
+}
+
+
+// Join in enclave: assumes that the records have been sorted
+// by the join attribute already
+// This method takes in a temporay row (which could be a dummy row)
+// Then it compares with the following rows (which should contain the row info, as well as the table info
+
+// TODO: format of the rows?
+// TODO: how to determine an upper bound ??
+// TODO: how to determine the output size?
+void sort_join(uint8_t *enc_temp_row, uint32_t enc_temp_row_len,
+			   uint8_t *input, uint32_t buffer_length, uint32_t list_length,
+			   uint8_t *output, uint32_t output_length) {
+  uint8_t *temp_row = (uint8_t *) malloc(ROW_UPPER_BOUND);
+  decrypt(enc_temp_row, enc_temp_row_len, temp_row);
+
+  uint8_t *temp_output = list_length;
+  // iterate through the rows, and join
+  
+  
 }
