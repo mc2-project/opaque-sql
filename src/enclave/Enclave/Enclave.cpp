c707cd90d231489b87b53d850ffa1895d05c180d
Ankur Dave <ankurdave@gmail.com>
2017-06-03 13:44:16 -0700

    Fix infinite loop

--- src/enclave/Enclave/Enclave.cpp
+++ src/enclave/Enclave/Enclave.cpp
@@ -106,7 +106,7 @@ void ecall_non_oblivious_aggregate_step1(
   uint8_t *input_rows, size_t input_rows_length,
   uint8_t **first_row, size_t *first_row_length,
   uint8_t **last_group, size_t *last_group_length) {
-  ecall_non_oblivious_aggregate_step1(
+  non_oblivious_aggregate_step1(
     agg_op, agg_op_length,
     input_rows, input_rows_length,
     first_row, first_row_length,
@@ -119,7 +119,7 @@ void ecall_non_oblivious_aggregate_step2(
   uint8_t *next_partition_first_row, size_t next_partition_first_row_length,
   uint8_t *prev_partition_last_group, size_t prev_partition_last_group_length,
   uint8_t **output_rows, size_t *output_rows_length) {
-  ecall_non_oblivious_aggregate_step2(
+  non_oblivious_aggregate_step2(
     agg_op, agg_op_length,
     input_rows, input_rows_length,
     next_partition_first_row, next_partition_first_row_length,
