d807b750c56de864278617abb15d6b2007c804a1
Ankur Dave <ankurdave@gmail.com>
2018-12-01 09:05:34 +0000

    Explicitly check for a 1 return value from sgx_is_outside_enclave

--- src/enclave/Enclave/util.cpp
+++ src/enclave/Enclave/util.cpp
@@ -43,7 +43,7 @@ void ocall_malloc(size_t size, uint8_t **ret) {
   unsafe_ocall_malloc(size, ret);
 
   // Guard against overwriting enclave memory
-  assert(sgx_is_outside_enclave(*ret, size));
+  assert(sgx_is_outside_enclave(*ret, size) == 1);
   sgx_lfence();
 }
 
