f2e6b8e36c18370ba410ae3a4f06b13c5cc11276
Chester Leung <chestercleung@gmail.com>
2019-03-24 22:00:13 -0700

    test

--- src/enclave/Enclave/ColumnSort.cpp
+++ src/enclave/Enclave/ColumnSort.cpp
@@ -111,7 +111,7 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
     printf("looking for this index: %i\n", i % num_partitions);
     FlatbuffersRowWriter* rw = ws[i % num_partitions].get();
     printf("found a row writer\n");
-    rw.write(row);
+    rw->write(row);
     printf("wrote a row\n");
     i++;
   }
