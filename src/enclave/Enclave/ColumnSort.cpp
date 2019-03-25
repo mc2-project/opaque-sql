928b18795e1776fc297eb2fcdaef8de867df8de3
Chester Leung <chestercleung@gmail.com>
2019-03-25 00:14:32 -0700

    test

--- src/enclave/Enclave/ColumnSort.cpp
+++ src/enclave/Enclave/ColumnSort.cpp
@@ -109,14 +109,14 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
     ws[i % num_partitions].write(row);
     i++;
   }
-  printf("wrote to all corresponding row writers");
+  printf("wrote to all corresponding row writers\n");
 
   FlatbuffersRowWriter shuffle_output_writer;
   for (uint32_t j = 0; j < num_partitions; j++) {
     ws[j].write_shuffle_output(ws[j].write_encrypted_blocks(), j);
-    printf("117");
     std::unique_ptr<uint8_t, decltype(&ocall_free)> out_buffer = ws[j].output_buffer();
-    printf("118");
+    uint32_t size = ws[j].output_size();
+    printf("size: %d", size);
     ShuffleOutputReader sor(out_buffer.get(), ws[j].output_size());
     printf("120");
     shuffle_output_writer.append_shuffle_output(sor.get());
