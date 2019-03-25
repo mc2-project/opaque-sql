710f6c584cfb0ece6bf55fe769c3a427886db1aa
Chester Leung <chestercleung@gmail.com>
2019-03-25 00:20:17 -0700

    test

--- src/enclave/Enclave/ColumnSort.cpp
+++ src/enclave/Enclave/ColumnSort.cpp
@@ -95,8 +95,6 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
               uint32_t partition_idx, uint32_t num_partitions,
               uint8_t **output_row, size_t *output_row_size) {
   (void)partition_idx;
-  (void)output_row;
-  (void)output_row_size;
 
   EncryptedBlocksToRowReader r(input_rows, input_rows_length);
 
