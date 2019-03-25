4a4c0dd306c81216f391a2105b809f8ada42ad3b
Chester Leung <chestercleung@gmail.com>
2019-03-24 23:44:18 -0700

    test

--- src/enclave/Enclave/ColumnSort.cpp
+++ src/enclave/Enclave/ColumnSort.cpp
@@ -100,16 +100,19 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
   std::vector<std::unique_ptr<FlatbuffersRowWriter>> ws(num_partitions);
 
   for (uint32_t k = 0; k < num_partitions; k++) {
-    ws.emplace_back(std::unique_ptr<FlatbuffersRowWriter>());
+    ws.emplace_back(std::unique_ptr<FlatbuffersRowWriter>(
+      new FlatbuffersRowWriter()));
   } 
   uint32_t i = 0;
 
   while (r.has_next()) {
     const tuix::Row *row = r.next();
     printf("looking for this index: %i\n", i % num_partitions);
-    FlatbuffersRowWriter* rw = ws[i % num_partitions].get();
-    rw->write(row);
-    printf("wrote row\n");
+    // FlatbuffersRowWriter* rw = ws[i % num_partitions].get();
+    // rw->write(row);
+    FlatbuffersRowWriter w;
+    w.write(row);
+    printf("wrote row\n")
     i++;
   }
   printf("wrote to all corresponding row writers");
