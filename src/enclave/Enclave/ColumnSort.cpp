32974dd16bdde335369c54e69a5f2e55ab731fa1
Chester Leung <chestercleung@gmail.com>
2019-03-25 23:07:14 -0700

    test

--- src/enclave/Enclave/ColumnSort.cpp
+++ src/enclave/Enclave/ColumnSort.cpp
@@ -105,6 +105,8 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
   while (r.has_next()) {
     const tuix::Row *row = r.next();
     ws[i % num_partitions].write(row);
+    print(row);
+    printf(" going to %d partition", i % num_partitions);
     i++;
   }
 
@@ -112,7 +114,7 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
   for (uint32_t j = 0; j < num_partitions; j++) {
     ws[j].write_shuffle_output(ws[j].write_encrypted_blocks(), j);
     std::unique_ptr<uint8_t, decltype(&ocall_free)> out_buffer = ws[j].output_buffer();
-    printf("\nBuffer: %s", out_buffer.get());
+    printf("\nBuffer: %s\n", out_buffer.get());
     // buffer is messed up
     ShuffleOutputReader sor(out_buffer.get(), ws[j].output_size());
     printf("120");
