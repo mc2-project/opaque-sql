c386aea6b7b451e597990e17598bbff1b4cf09b9
Chester Leung <chestercleung@gmail.com>
2019-03-25 22:57:30 -0700

    restore std unique ptr

--- src/enclave/Enclave/ColumnSort.cpp
+++ src/enclave/Enclave/ColumnSort.cpp
@@ -111,8 +111,9 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
   FlatbuffersRowWriter shuffle_output_writer;
   for (uint32_t j = 0; j < num_partitions; j++) {
     ws[j].write_shuffle_output(ws[j].write_encrypted_blocks(), j);
-    // std::unique_ptr<uint8_t, decltype(&ocall_free)> out_buffer = ws[j].output_buffer();
-    ShuffleOutputReader sor(ws[j].output_buffer().release(), ws[j].output_size());
+    std::unique_ptr<uint8_t, decltype(&ocall_free)> out_buffer = ws[j].output_buffer();
+    // buffer is messed up
+    ShuffleOutputReader sor(out_buffer.get(), ws[j].output_size());
     printf("120");
     shuffle_output_writer.append_shuffle_output(sor.get());
   }
