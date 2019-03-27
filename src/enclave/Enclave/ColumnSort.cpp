4fbaffa55e8d8eefbd9cc55d64098c87876ccc50
Chester Leung <chestercleung@gmail.com>
2019-03-26 21:32:19 -0700

    debugging

--- src/enclave/Enclave/ColumnSort.cpp
+++ src/enclave/Enclave/ColumnSort.cpp
@@ -110,18 +110,18 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
   while (r.has_next()) {
     const tuix::Row *row = r.next();
     printf("Attempting to write row");
-    ws[i % num_partitions]->write(row);
+    ws[i % num_partitions].write(row);
     printf("Finished writing row\n");
     i++;
   }
 
   FlatbuffersRowWriter shuffle_output_writer;
   for (uint32_t j = 0; j < num_partitions; j++) {
-    ws[j]->write_shuffle_output(ws[j]->write_encrypted_blocks(), j);
-    std::unique_ptr<uint8_t, decltype(&ocall_free)> out_buffer = ws[j]->output_buffer();
+    ws[j]->write_shuffle_output(ws[j].write_encrypted_blocks(), j);
+    std::unique_ptr<uint8_t, decltype(&ocall_free)> out_buffer = ws[j].output_buffer();
     printf("\nBuffer: %p\n", out_buffer.get());
     // buffer is messed up
-    ShuffleOutputReader sor(out_buffer.get(), ws[j]->output_size());
+    ShuffleOutputReader sor(out_buffer.get(), ws[j].output_size());
     printf("120");
     shuffle_output_writer.append_shuffle_output(sor.get());
   }
