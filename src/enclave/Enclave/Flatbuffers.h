b232f3993850ab48bf84f2a9f959740163aea932
Chester Leung <chestercleung@gmail.com>
2019-03-26 21:57:43 -0700

    debug

--- src/enclave/Enclave/Flatbuffers.h
+++ src/enclave/Enclave/Flatbuffers.h
@@ -436,8 +436,6 @@ public:
     builder.Finish(tuix::CreateRowsDirect(builder, &rows_vector));
     size_t enc_rows_len = enc_size(builder.GetSize());
 
-    printf("enc_rows_len = %i", enc_rows_len);
-
     uint8_t *enc_rows_ptr = nullptr;
     ocall_malloc(enc_rows_len, &enc_rows_ptr);
 
@@ -456,6 +454,7 @@ public:
 
   flatbuffers::Offset<tuix::EncryptedBlocks> write_encrypted_blocks() {
     if (rows_vector.size() > 0) {
+      printf("rows_vector size: %i\n", rows_vector.size());
       write_encrypted_block();
     }
     auto result = tuix::CreateEncryptedBlocksDirect(enc_block_builder, &enc_block_vector);
