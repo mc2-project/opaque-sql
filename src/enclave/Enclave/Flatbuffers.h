bf77be3230babb171df6d9eb67201f42b6c68e3f
Chester Leung <chestercleung@gmail.com>
2019-03-27 16:18:55 -0700

    test pad

--- src/enclave/Enclave/Flatbuffers.h
+++ src/enclave/Enclave/Flatbuffers.h
@@ -396,7 +396,7 @@ public:
       field_values[i] = flatbuffers_copy<tuix::Field>(
         row->field_values()->Get(i), builder, true);
     }
-    rows_vector.push_back(tuix::CreateRow(builder, &field_values, true));
+    rows_vector.push_back(tuix::CreateRowDirect(builder, &field_values, true));
     total_num_rows++;
     maybe_finish_block();
   }
