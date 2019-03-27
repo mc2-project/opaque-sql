737b23c25aba10715c6c9793c8b4989c25fd77bd
Chester Leung <chestercleung@gmail.com>
2019-03-27 16:32:14 -0700

    test pad

--- src/main/scala/edu/berkeley/cs/rise/opaque/execution/ObliviousSortExec.scala
+++ src/main/scala/edu/berkeley/cs/rise/opaque/execution/ObliviousSortExec.scala
@@ -154,7 +154,7 @@ object ObliviousSortExec extends java.io.Serializable {
     //   .mapPartitions(pairIter => Iterator(Utils.concatEncryptedBlocks(pairIter.flatMap(_._2).toSeq)))
 
     // Filter out dummy rows
-    val filtered_data = sorted_data.map(x => ColumnSortFilter(x, sort_order, r, s))
+    val filtered_data = shifted_up_data.map(x => ColumnSortFilter(x, sort_order, r, s))
 
     filtered_data
   }
