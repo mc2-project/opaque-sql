719a7569f5530bd993c2c4025fbe355735602c6e
Chester Leung <chestercleung@gmail.com>
2019-03-28 00:15:28 -0700

    debug

--- src/enclave/Enclave/ColumnSort.cpp
+++ src/enclave/Enclave/ColumnSort.cpp
@@ -121,6 +121,10 @@ void transpose(uint8_t *input_rows, uint32_t input_rows_length,
 
   while (r.has_next()) {
     const tuix::Row *row = r.next();
+    if (row->is_dummy) {
+      printf("\nDUMMY ROW: ");
+      print(row);
+    }
     ws[i % num_partitions]->write(row);
     i++;
   }
