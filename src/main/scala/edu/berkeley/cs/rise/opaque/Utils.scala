91301a9dc9a662eb17c6beee424b73e308fa7bbb
Chester Leung <chestercleung@gmail.com>
2019-03-27 17:01:22 -0700

    Add concatEncryptedBlocksWithIndex to sort last partition properly

--- src/main/scala/edu/berkeley/cs/rise/opaque/Utils.scala
+++ src/main/scala/edu/berkeley/cs/rise/opaque/Utils.scala
@@ -1191,6 +1191,35 @@ object Utils extends Logging {
     Block(builder.sizedByteArray())
   }
 
+  def concatEncryptedBlocksWithIndex(last_partition: Boolean, blocks: Seq[Block]): Block = {
+    if (last_partition) {
+      val allBlocks = for {
+        block <- blocks
+        encryptedBlocks = tuix.EncryptedBlocks.getRootAsEncryptedBlocks(ByteBuffer.wrap(block.bytes))
+        i <- 0 until encryptedBlocks.blocksLength by -1
+      } yield encryptedBlocks.blocks(i)
+    } else {
+      val allBlocks = for {
+        block <- blocks
+        encryptedBlocks = tuix.EncryptedBlocks.getRootAsEncryptedBlocks(ByteBuffer.wrap(block.bytes))
+        i <- 0 until encryptedBlocks.blocksLength
+      } yield encryptedBlocks.blocks(i)
+    }
+
+    val builder = new FlatBufferBuilder
+    builder.finish(
+      tuix.EncryptedBlocks.createEncryptedBlocks(
+        builder, tuix.EncryptedBlocks.createBlocksVector(builder, allBlocks.map { encryptedBlock =>
+          val encRows = new Array[Byte](encryptedBlock.encRowsLength)
+          encryptedBlock.encRowsAsByteBuffer.get(encRows)
+          tuix.EncryptedBlock.createEncryptedBlock(
+            builder,
+            encryptedBlock.numRows,
+            tuix.EncryptedBlock.createEncRowsVector(builder, encRows))
+        }.toArray)))
+    Block(builder.sizedByteArray())
+  }
+
   def emptyBlock: Block = {
     val builder = new FlatBufferBuilder
     builder.finish(
