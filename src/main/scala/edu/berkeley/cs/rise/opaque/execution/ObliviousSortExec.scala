c1ecf877ae1f93cb5b1cbd96224c2473db7bdba3
Chester Leung <chestercleung@gmail.com>
2019-04-01 04:31:50 -0700

    This works for even rows. yay

--- src/main/scala/edu/berkeley/cs/rise/opaque/execution/ObliviousSortExec.scala
+++ src/main/scala/edu/berkeley/cs/rise/opaque/execution/ObliviousSortExec.scala
@@ -147,12 +147,12 @@ object ObliviousSortExec extends java.io.Serializable {
       .groupByKey()
       .mapPartitions(pairIter => Iterator(Utils.concatEncryptedBlocks(pairIter.flatMap(_._2).toSeq)))
 
-    // // Final oblivious sort
-    // val sorted_data = shifted_up_data.mapPartitionsWithIndex {
-    //   (index, l) => l.map(x => ExternalSort(x, sort_order))}
+    // Final oblivious sort
+    val sorted_data = shifted_up_data.mapPartitionsWithIndex {
+      (index, l) => l.map(x => ExternalSort(x, sort_order))}
 
     // Filter out dummy rows
-    val filtered_data = shifted_up_data.map(x => ColumnSortFilter(x.bytes, sort_order, r, s))
+    val filtered_data = sorted_data.map(x => ColumnSortFilter(x, sort_order, r, s))
 
     filtered_data
   }
