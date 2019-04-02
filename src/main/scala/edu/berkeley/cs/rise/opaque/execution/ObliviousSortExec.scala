3e702623ea085503885e57858531318000812936
Chester Leung <chestercleung@gmail.com>
2019-04-02 14:25:25 -0700

    revert

--- src/main/scala/edu/berkeley/cs/rise/opaque/execution/ObliviousSortExec.scala
+++ src/main/scala/edu/berkeley/cs/rise/opaque/execution/ObliviousSortExec.scala
@@ -152,7 +152,7 @@ object ObliviousSortExec extends java.io.Serializable {
       (index, l) => l.map(x => ExternalSort(x, sort_order))}
 
     // Filter out dummy rows
-    val filtered_data = shifted_up_data.map(x => ColumnSortFilter(x.bytes, sort_order, r, s))
+    val filtered_data = sorted_data.map(x => ColumnSortFilter(x, sort_order, r, s))
 
     filtered_data
   }
