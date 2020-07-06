0fecefdb50c198643c48fdb6f1e39fb942b6c1d9
Chester Leung <chester.leung@berkeley.edu>
2020-07-06 22:28:39 +0000

    Add test scripts and sample serialized dataframes

---
+++ load.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.sql.types._
+
+val df = (spark.read.format("edu.berkeley.cs.rise.opaque.EncryptedSource")
+  .schema(StructType(Seq(StructField("word", StringType), StructField("count", IntegerType))))
+  .load("dfEncrypted"))
+println("dfEncrypted")
+df.show()
+
+val df2 = (spark.read.format("edu.berkeley.cs.rise.opaque.EncryptedSource")
+  .schema(StructType(Seq(StructField("word", StringType), StructField("count", IntegerType))))
+  .load("dfEncrypted2"))
+println("dfEncrypted2")
+df2.show()
+
