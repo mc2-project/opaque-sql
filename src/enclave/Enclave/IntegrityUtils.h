a9f4994a1197f02c04e28c23b36cf50be5f66ea1
Chester Leung <chester.leung@berkeley.edu>
2020-10-15 18:40:23 +0000

    Add missing file, remove extra functions from IntegrityUtils

--- src/enclave/Enclave/IntegrityUtils.h
+++ src/enclave/Enclave/IntegrityUtils.h
@@ -3,10 +3,6 @@
 
 using namespace edu::berkeley::cs::rise::opaque;
 
-void init_log(const tuix::EncryptedBlocks *encrypted_blocks);
-void verify_log(const tuix::EncryptedBlocks *encrypted_blocks, 
-    std::vector<LogEntry> past_log_entries);
-
 void mac_log_entry_chain(int num_bytes_to_mac, uint8_t* to_mac, uint8_t* global_mac, 
     std::string curr_ecall, int curr_pid, int rcv_pid, int job_id, int num_macs, 
     int num_past_entries, std::vector<LogEntry> past_log_entries, int first_le_index, 
