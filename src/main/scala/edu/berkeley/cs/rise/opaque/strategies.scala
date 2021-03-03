6aa1025763238a98e19026a6b459bb53611a45d4
Octavian Sima <octavian.sima98@gmail.com>
2021-03-03 22:13:22 +0000

    whitespace in strategies.scala

--- src/main/scala/edu/berkeley/cs/rise/opaque/strategies.scala
+++ src/main/scala/edu/berkeley/cs/rise/opaque/strategies.scala
@@ -80,7 +80,8 @@ object OpaqueOperators extends Strategy {
       EncryptedSortExec(sortExprs, global, planLater(child)) :: Nil
 
     // Used to match equi joins
-    case p @ ExtractEquiJoinKeys(joinType, leftKeys, rightKeys, condition, left, right, _) if isEncrypted(p) =>
+    case p @ ExtractEquiJoinKeys(joinType, leftKeys, rightKeys, condition, left, right, _)
+        if isEncrypted(p) =>
       val (leftProjSchema, leftKeysProj, tag) = tagForJoin(leftKeys, left.output, true)
       val (rightProjSchema, rightKeysProj, _) = tagForJoin(rightKeys, right.output, false)
       val leftProj = EncryptedProjectExec(leftProjSchema, planLater(left))
@@ -98,8 +99,9 @@ object OpaqueOperators extends Strategy {
         rightKeysProj,
         leftProjSchema.map(_.toAttribute),
         rightProjSchema.map(_.toAttribute),
-        condition, 
-        sorted)
+        condition,
+        sorted
+      )
 
       val tagsDropped = joinType match {
         case Inner => EncryptedProjectExec(dropTags(left.output, right.output), joined)
@@ -109,14 +111,17 @@ object OpaqueOperators extends Strategy {
       tagsDropped :: Nil
 
     // Used to match non-equi joins
-    case Join(left, right, joinType, condition, hint) if isEncrypted(left) && isEncrypted(right) =>
+    case Join(left, right, joinType, condition, hint)
+        if isEncrypted(left) && isEncrypted(right) =>
       // How to pick broadcast side: if left join, broadcast right. If right join, broadcast left.
       // This is the simplest and most performant method, but may be worth revisting if one side is
       // significantly smaller than the other. Otherwise, pick the smallest side to broadcast.
       // NOTE: the current implementation of BNLJ only works under the assumption that
       // left join <==> broadcast right AND right join <==> broadcast left.
-      val desiredBuildSide = if (joinType.isInstanceOf[InnerLike] || joinType == FullOuter)
-          getSmallerSide(left, right) else
+      val desiredBuildSide =
+        if (joinType.isInstanceOf[InnerLike] || joinType == FullOuter)
+          getSmallerSide(left, right)
+        else
           getBroadcastSideBNLJ(joinType)
 
       val joined = EncryptedBroadcastNestedLoopJoinExec(
@@ -124,37 +129,68 @@ object OpaqueOperators extends Strategy {
         planLater(right),
         desiredBuildSide,
         joinType,
-        condition)
+        condition
+      )
 
       joined :: Nil
 
     case a @ PhysicalAggregation(groupingExpressions, aggExpressions, resultExpressions, child)
-        if (isEncrypted(child) && aggExpressions.forall(expr => expr.isInstanceOf[AggregateExpression])) =>
-
-      val aggregateExpressions = aggExpressions.map(expr => expr.asInstanceOf[AggregateExpression])
-      val (functionsWithDistinct, functionsWithoutDistinct) = aggregateExpressions.partition(_.isDistinct)
+        if (isEncrypted(child) && aggExpressions
+          .forall(expr => expr.isInstanceOf[AggregateExpression])) =>
+      val aggregateExpressions =
+        aggExpressions.map(expr => expr.asInstanceOf[AggregateExpression])
+      val (functionsWithDistinct, functionsWithoutDistinct) =
+        aggregateExpressions.partition(_.isDistinct)
 
       functionsWithDistinct.size match {
         case 0 => // No distinct aggregate operations
           if (groupingExpressions.size == 0) {
             // Global aggregation
-            val partialAggregate = EncryptedAggregateExec(groupingExpressions,
-              aggregateExpressions.map(_.copy(mode = Partial)), planLater(child))
+            val partialAggregate = EncryptedAggregateExec(
+              groupingExpressions,
+              aggregateExpressions.map(_.copy(mode = Partial)),
+              planLater(child)
+            )
             val partialOutput = partialAggregate.output
             val (projSchema, tag) = tagForGlobalAggregate(partialOutput)
 
-            EncryptedProjectExec(resultExpressions, 
-              EncryptedAggregateExec(groupingExpressions, aggregateExpressions.map(_.copy(mode = Final)),
-                EncryptedProjectExec(partialOutput,
-                  EncryptedSortExec(Seq(SortOrder(tag, Ascending)), true, 
-                    EncryptedProjectExec(projSchema, partialAggregate))))) :: Nil
+            EncryptedProjectExec(
+              resultExpressions,
+              EncryptedAggregateExec(
+                groupingExpressions,
+                aggregateExpressions.map(_.copy(mode = Final)),
+                EncryptedProjectExec(
+                  partialOutput,
+                  EncryptedSortExec(
+                    Seq(SortOrder(tag, Ascending)),
+                    true,
+                    EncryptedProjectExec(projSchema, partialAggregate)
+                  )
+                )
+              )
+            ) :: Nil
           } else {
             // Grouping aggregation
-            EncryptedProjectExec(resultExpressions,
-              EncryptedAggregateExec(groupingExpressions, aggregateExpressions.map(_.copy(mode = Final)),
-                EncryptedSortExec(groupingExpressions.map(_.toAttribute).map(e => SortOrder(e, Ascending)), true,
-                  EncryptedAggregateExec(groupingExpressions, aggregateExpressions.map(_.copy(mode = Partial)),
-                    EncryptedSortExec(groupingExpressions.map(e => SortOrder(e, Ascending)), false, planLater(child)))))) :: Nil
+            EncryptedProjectExec(
+              resultExpressions,
+              EncryptedAggregateExec(
+                groupingExpressions,
+                aggregateExpressions.map(_.copy(mode = Final)),
+                EncryptedSortExec(
+                  groupingExpressions.map(_.toAttribute).map(e => SortOrder(e, Ascending)),
+                  true,
+                  EncryptedAggregateExec(
+                    groupingExpressions,
+                    aggregateExpressions.map(_.copy(mode = Partial)),
+                    EncryptedSortExec(
+                      groupingExpressions.map(e => SortOrder(e, Ascending)),
+                      false,
+                      planLater(child)
+                    )
+                  )
+                )
+              )
+            ) :: Nil
           }
         case size if size == 1 => // One distinct aggregate operation
           // Because we are also grouping on the columns used in the distinct expressions,
@@ -162,22 +198,31 @@ object OpaqueOperators extends Strategy {
 
           // We need to extract named expressions from the children of the distinct aggregate functions
           // in order to group by those columns.
-          val namedDistinctExpressions = functionsWithDistinct.head.aggregateFunction.children.flatMap{ e =>
-            e match {
-              case ne: NamedExpression =>
-                Seq(ne)
-              case _ =>
-                e.children.filter(child => child.isInstanceOf[NamedExpression])
-                  .map(child => child.asInstanceOf[NamedExpression])
+          val namedDistinctExpressions =
+            functionsWithDistinct.head.aggregateFunction.children.flatMap { e =>
+              e match {
+                case ne: NamedExpression =>
+                  Seq(ne)
+                case _ =>
+                  e.children
+                    .filter(child => child.isInstanceOf[NamedExpression])
+                    .map(child => child.asInstanceOf[NamedExpression])
+              }
             }
-          }
           val combinedGroupingExpressions = groupingExpressions ++ namedDistinctExpressions
 
           // 1. Create an Aggregate operator for partial aggregations.
           val partialAggregate = {
-            val sorted = EncryptedSortExec(combinedGroupingExpressions.map(e => SortOrder(e, Ascending)), false,
-              planLater(child))
-            EncryptedAggregateExec(combinedGroupingExpressions, functionsWithoutDistinct.map(_.copy(mode = Partial)), sorted)
+            val sorted = EncryptedSortExec(
+              combinedGroupingExpressions.map(e => SortOrder(e, Ascending)),
+              false,
+              planLater(child)
+            )
+            EncryptedAggregateExec(
+              combinedGroupingExpressions,
+              functionsWithoutDistinct.map(_.copy(mode = Partial)),
+              sorted
+            )
           }
 
           // 2. Create an Aggregate operator for partial merge aggregations.
@@ -190,60 +235,79 @@ object OpaqueOperators extends Strategy {
             val sortOrder = combinedGroupingExpressions.map(e => SortOrder(e, Ascending))
             val sorted = EncryptedSortExec(sortOrder, false, partitioned)
 
-            EncryptedAggregateExec(combinedGroupingExpressions,
-              functionsWithoutDistinct.map(_.copy(mode = PartialMerge)), sorted)
+            EncryptedAggregateExec(
+              combinedGroupingExpressions,
+              functionsWithoutDistinct.map(_.copy(mode = PartialMerge)),
+              sorted
+            )
           }
 
           // 3. Create an Aggregate operator for partial aggregation of distinct aggregate expressions.
           val partialDistinctAggregate = {
             // Indistinct functions operate on aggregation buffers since partial aggregation was already called,
             // but distinct functions operate on the original input to the aggregation.
-            EncryptedAggregateExec(groupingExpressions,
+            EncryptedAggregateExec(
+              groupingExpressions,
               functionsWithoutDistinct.map(_.copy(mode = PartialMerge)) ++
-                functionsWithDistinct.map(_.copy(mode = Partial)), partialMergeAggregate)
+                functionsWithDistinct.map(_.copy(mode = Partial)),
+              partialMergeAggregate
+            )
           }
 
           // 4. Create an Aggregate operator for the final aggregation.
           val finalAggregate = {
-            val sorted = EncryptedSortExec(groupingExpressions.map(e => SortOrder(e, Ascending)),
-              true, partialDistinctAggregate)
-            EncryptedAggregateExec(groupingExpressions,
-                (functionsWithoutDistinct ++ functionsWithDistinct).map(_.copy(mode = Final)), sorted)
+            val sorted = EncryptedSortExec(
+              groupingExpressions.map(e => SortOrder(e, Ascending)),
+              true,
+              partialDistinctAggregate
+            )
+            EncryptedAggregateExec(
+              groupingExpressions,
+              (functionsWithoutDistinct ++ functionsWithDistinct).map(_.copy(mode = Final)),
+              sorted
+            )
           }
 
           EncryptedProjectExec(resultExpressions, finalAggregate) :: Nil
 
         case _ => { // More than one distinct operations
-          throw new UnsupportedOperationException("Aggregate operations with more than one distinct expressions are not yet supported.")
+          throw new UnsupportedOperationException(
+            "Aggregate operations with more than one distinct expressions are not yet supported."
+          )
         }
       }
 
     case p @ Union(Seq(left, right)) if isEncrypted(p) =>
       EncryptedUnionExec(planLater(left), planLater(right)) :: Nil
 
-    case ReturnAnswer(rootPlan) => rootPlan match {
-      case Limit(IntegerLiteral(limit), Sort(sortExprs, true, child)) if isEncrypted(child) =>
-      EncryptedGlobalLimitExec(limit,
-        EncryptedLocalLimitExec(limit,
-          EncryptedSortExec(sortExprs, true, planLater(child)))) :: Nil
+    case ReturnAnswer(rootPlan) =>
+      rootPlan match {
+        case Limit(IntegerLiteral(limit), Sort(sortExprs, true, child)) if isEncrypted(child) =>
+          EncryptedGlobalLimitExec(
+            limit,
+            EncryptedLocalLimitExec(limit, EncryptedSortExec(sortExprs, true, planLater(child)))
+          ) :: Nil
 
-      case Limit(IntegerLiteral(limit), Project(projectList, child)) if isEncrypted(child) =>
-        EncryptedGlobalLimitExec(limit,
-          EncryptedLocalLimitExec(limit,
-            EncryptedProjectExec(projectList, planLater(child)))) :: Nil
+        case Limit(IntegerLiteral(limit), Project(projectList, child)) if isEncrypted(child) =>
+          EncryptedGlobalLimitExec(
+            limit,
+            EncryptedLocalLimitExec(limit, EncryptedProjectExec(projectList, planLater(child)))
+          ) :: Nil
 
-      case _ => Nil
-    }
+        case _ => Nil
+      }
 
     case Limit(IntegerLiteral(limit), Sort(sortExprs, true, child)) if isEncrypted(child) =>
-      EncryptedGlobalLimitExec(limit,
-        EncryptedLocalLimitExec(limit,
-          EncryptedSortExec(sortExprs, true, planLater(child)))) :: Nil
+      EncryptedGlobalLimitExec(
+        limit,
+        EncryptedLocalLimitExec(limit, EncryptedSortExec(sortExprs, true, planLater(child)))
+      ) :: Nil
 
     case Limit(IntegerLiteral(limit), Project(projectList, child)) if isEncrypted(child) =>
-      EncryptedGlobalLimitExec(limit,
-        EncryptedLocalLimitExec(limit,
-          EncryptedProjectExec(projectList, planLater(child)))) :: Nil
+      EncryptedGlobalLimitExec(
+        limit,
+        EncryptedLocalLimitExec(limit, EncryptedProjectExec(projectList, planLater(child)))
+      ) :: Nil
 
     case LocalLimit(IntegerLiteral(limit), child) if isEncrypted(child) =>
       EncryptedLocalLimitExec(limit, planLater(child)) :: Nil
@@ -264,23 +328,31 @@ object OpaqueOperators extends Strategy {
   }
 
   private def tagForJoin(
-      keys: Seq[Expression], input: Seq[Attribute], isLeft: Boolean)
-    : (Seq[NamedExpression], Seq[NamedExpression], NamedExpression) = {
+      keys: Seq[Expression],
+      input: Seq[Attribute],
+      isLeft: Boolean
+  ): (Seq[NamedExpression], Seq[NamedExpression], NamedExpression) = {
     val keysProj = keys.zipWithIndex.map { case (k, i) => Alias(k, "_" + i)() }
     val tag = Alias(Literal(if (isLeft) 0 else 1), "_tag")()
     (Seq(tag) ++ keysProj ++ input, keysProj.map(_.toAttribute), tag.toAttribute)
   }
 
   private def dropTags(
-      leftOutput: Seq[Attribute], rightOutput: Seq[Attribute]): Seq[NamedExpression] =
+      leftOutput: Seq[Attribute],
+      rightOutput: Seq[Attribute]
+  ): Seq[NamedExpression] =
     leftOutput ++ rightOutput
 
   private def sortForJoin(
-      leftKeys: Seq[Expression], tag: Expression, input: Seq[Attribute]): Seq[SortOrder] =
+      leftKeys: Seq[Expression],
+      tag: Expression,
+      input: Seq[Attribute]
+  ): Seq[SortOrder] =
     leftKeys.map(k => SortOrder(k, Ascending)) :+ SortOrder(tag, Ascending)
 
-  private def tagForGlobalAggregate(input: Seq[Attribute])
-      : (Seq[NamedExpression], NamedExpression) = {
+  private def tagForGlobalAggregate(
+      input: Seq[Attribute]
+  ): (Seq[NamedExpression], NamedExpression) = {
     val tag = Alias(Literal(0), "_tag")()
     (Seq(tag) ++ input, tag.toAttribute)
   }
