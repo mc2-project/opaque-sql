f860eef979788668e27206768663049a4dd50da9
Octavian Sima <octavian.sima98@gmail.com>
2021-03-18 22:10:34 +0000

    create suite started

---
+++ src/test/scala/edu/berkeley/cs/rise/opaque/CreateSuite.scala
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.berkeley.cs.rise.opaque
+
+import java.sql.Timestamp
+
+import scala.util.Random
+
+import org.apache.spark.SparkException
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.SQLImplicits
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.unsafe.types.CalendarInterval
+
+import edu.berkeley.cs.rise.opaque.benchmark._
+import edu.berkeley.cs.rise.opaque.execution.EncryptedBlockRDDScanExec
+import edu.berkeley.cs.rise.opaque.expressions.Decrypt.decrypt
+import edu.berkeley.cs.rise.opaque.expressions.DotProduct.dot
+import edu.berkeley.cs.rise.opaque.expressions.VectorMultiply.vectormultiply
+import edu.berkeley.cs.rise.opaque.expressions.VectorSum
+
+/* Creates DataFrames with various types */
+trait CreateSuite extends OpaqueSuiteBase with SQLHelper {
+  import spark.implicits._
+
+  test("Interval SQL") {
+    def createData(sl: SecurityLevel) = {
+      val data = Seq(Tuple2(1, new java.sql.Date(new java.util.Date().getTime())))
+      val df = sl.applyTo(data.toDF("index", "time"))
+      df.createOrReplaceTempView("Interval")
+      df
+    }
+    checkAnswer() { sl =>
+      createData(sl)
+      spark.sql("SELECT time + INTERVAL 7 DAY FROM Interval")
+    }
+
+    checkAnswer() { sl =>
+      createData(sl)
+      spark.sql("SELECT time + INTERVAL 7 WEEK FROM Interval")
+    }
+
+    checkAnswer() { sl =>
+      createData(sl)
+      spark.sql("SELECT time + INTERVAL 6 MONTH FROM Interval")
+    }
+
+    checkAnswer() { sl =>
+      val df = createData(sl)
+      df.select(date_add($"time", 3))
+    }
+
+    safeDropTables("Interval")
+  }
+}
+
+class SinglePartitionCreateSuite extends CreateSuite with SinglePartitionSparkSession {}
+
+class MultiplePartitionCreateSuite extends CreateSuite with MultiplePartitionSparkSession {}
---
+++ src/test/scala/edu/berkeley/cs/rise/opaque/MiscSuite.scala
@@ -0,0 +1,125 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.berkeley.cs.rise.opaque
+
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+import edu.berkeley.cs.rise.opaque.benchmark._
+import edu.berkeley.cs.rise.opaque.expressions.DotProduct.dot
+import edu.berkeley.cs.rise.opaque.expressions.VectorMultiply.vectormultiply
+import edu.berkeley.cs.rise.opaque.expressions.VectorSum
+
+/* Contains non-query related Spark tests */
+trait MiscSuite extends OpaqueSuiteBase {
+  import spark.implicits._
+
+  test("exp") {
+    checkAnswer() { sl =>
+      val data: Seq[(Double, Double)] = Seq((2.0, 3.0))
+      val schema = StructType(Seq(StructField("x", DoubleType), StructField("y", DoubleType)))
+
+      val df = sl.applyTo(
+        spark.createDataFrame(
+          spark.sparkContext.makeRDD(data.map(Row.fromTuple), numPartitions),
+          schema
+        )
+      )
+      df.select(exp($"y"))
+    }
+  }
+
+  test("vector multiply") {
+    checkAnswer() { sl =>
+      val data: Seq[(Array[Double], Double)] = Seq((Array[Double](1.0, 1.0, 1.0), 3.0))
+      val schema = StructType(
+        Seq(StructField("v", DataTypes.createArrayType(DoubleType)), StructField("c", DoubleType))
+      )
+
+      val df = sl.applyTo(
+        spark.createDataFrame(
+          spark.sparkContext.makeRDD(data.map(Row.fromTuple), numPartitions),
+          schema
+        )
+      )
+      df.select(vectormultiply($"v", $"c"))
+    }
+  }
+
+  test("dot product") {
+    checkAnswer() { sl =>
+      val data: Seq[(Array[Double], Array[Double])] =
+        Seq((Array[Double](1.0, 1.0, 1.0), Array[Double](1.0, 1.0, 1.0)))
+      val schema = StructType(
+        Seq(
+          StructField("v1", DataTypes.createArrayType(DoubleType)),
+          StructField("v2", DataTypes.createArrayType(DoubleType))
+        )
+      )
+
+      val df = sl.applyTo(
+        spark.createDataFrame(
+          spark.sparkContext.makeRDD(data.map(Row.fromTuple), numPartitions),
+          schema
+        )
+      )
+      df.select(dot($"v1", $"v2"))
+    }
+  }
+
+  test("vector sum") {
+    checkAnswer() { sl =>
+      val data: Seq[(Array[Double], Double)] =
+        Seq((Array[Double](1.0, 2.0, 3.0), 4.0), (Array[Double](5.0, 7.0, 7.0), 8.0))
+      val schema = StructType(
+        Seq(StructField("v", DataTypes.createArrayType(DoubleType)), StructField("c", DoubleType))
+      )
+
+      val df = sl.applyTo(
+        spark.createDataFrame(
+          spark.sparkContext.makeRDD(data.map(Row.fromTuple), numPartitions),
+          schema
+        )
+      )
+      val vectorsum = new VectorSum
+      df.groupBy().agg(vectorsum($"v"))
+    }
+  }
+
+  test("least squares") {
+    checkAnswer() { sl =>
+      LeastSquares.query(spark, sl, "tiny", numPartitions)
+    }
+  }
+
+  test("logistic regression") {
+    checkAnswer() { sl =>
+      LogisticRegression.train(spark, sl, 1000, numPartitions)
+    }
+  }
+  test("k-means") {
+    checkAnswer() { sl =>
+      KMeans.train(spark, sl, numPartitions, 10, 2, 3, 0.01).sortBy(_(0))
+    }
+  }
+}
+
+class SinglePartitionMiscSuite extends MiscSuite with SinglePartitionSparkSession {}
+
+class MultiplePartitionMiscSuite extends MiscSuite with MultiplePartitionSparkSession {}
