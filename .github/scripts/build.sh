c9ae36255b3ae449343e12b5d7f080bff82191b6
Octavian Sima <octavian.sima98@gmail.com>
2021-04-14 00:21:19 +0000

    reorganization

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -24,15 +24,17 @@ sudo mv spark-3.1.1*/* /opt/spark
 rm -rf spark-3.1.1*
 sudo mkdir /opt/spark/work
 sudo chmod -R a+wx /opt/spark/work
-touch /opt/spark/conf/spark-defaults.conf
-echo "spark.driver.bindAddress  127.0.0.1" >> /opt/spark/conf/spark-defaults.conf
 
 # Set Spark environment variables
 export SPARK_HOME=/opt/spark
 export PATH=$PATH:/opt/spark/bin:/opt/spark/sbin
 
-# Set hostname to be localhost, since only running build/sbt test
+# Set hostname to be localhost for Spark, since only running build/sbt test.
+# Fixes issues with assigning IP for sparkDriver on Github Actions VM.
 sudo hostname -b 127.0.0.1
+touch /opt/spark/conf/spark-defaults.conf
+echo "" >> /opt/spark/conf/spark-defaults.conf
+echo "spark.driver.bindAddress  127.0.0.1" >> /opt/spark/conf/spark-defaults.conf
 
 # Generate keypair for attestation
 openssl genrsa -out ./private_key.pem -3 3072
