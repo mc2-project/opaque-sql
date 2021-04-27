46418aae35f4e75eadccce087f244fd6d1063899
Eric Feng <fengeric11@gmail.com>
2021-04-27 03:49:41 +0000

    gRPC RA functions

--- src/main/scala/edu/berkeley/cs/rise/opaque/RA.scala
+++ src/main/scala/edu/berkeley/cs/rise/opaque/RA.scala
@@ -17,8 +17,11 @@
 
 package edu.berkeley.cs.rise.opaque
 
+import javax.xml.bind.DatatypeConverter
+
 import org.apache.spark.{SparkContext, SparkEnv}
 import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
 
 import edu.berkeley.cs.rise.opaque.execution.SP
 
@@ -38,14 +41,10 @@ object RA extends Logging {
     val rdd = sc.parallelize(Seq.fill(numExecutors) { () }, numExecutors)
 
     val intelCert = Utils.findResource("AttestationReportSigningCACert.pem")
+    val userCert = scala.io.Source.fromFile("/home/opaque/opaque/user1.crt").mkString
     val sp = new SP()
 
-    Utils.sharedKey match {
-      case Some(sharedKey) =>
-        sp.Init(sharedKey, intelCert)
-      case None =>
-        throw new OpaqueException("Cannot begin attestation without sharedKey.")
-    }
+    sp.Init(Utils.sharedKey, userCert)
 
     val numAttested = Utils.numAttested
     // Runs on executors
@@ -107,7 +106,9 @@ object RA extends Logging {
       logInfo(
         s"RA.run: ${Utils.numEnclaves.value} unattested, ${Utils.numAttested.value} attested"
       )
+
       initRA(sc)
+      LA.initLA(sc)
     }
     Thread.sleep(100)
   }
@@ -128,4 +129,68 @@ object RA extends Logging {
     Thread.sleep(5000)
   }
 
+// Helper functions for grpc RA
+
+  def printReport(): Unit = {
+
+    val sc = SparkSession.active.sparkContext
+    if (!sc.isLocal) {
+      numExecutors = sc.getConf.getInt("spark.executor.instances", -1)
+    }
+
+    val rdd = sc.parallelize(Seq.fill(numExecutors) {()}, numExecutors)
+
+    // Runs on executors
+    val msg1s = rdd.mapPartitions { (_) =>
+      // Need to reset attested boolean for client
+      Utils.attested = false
+
+      val (eid, msg1) = Utils.generateReport()
+      Iterator((eid, msg1))
+    }.collect.toMap
+
+    // Prints out report
+    var raReport: String = ""
+    var eidReport: String = ""
+
+    for ((eid, msg) <- msg1s) {
+      raReport = raReport + Utils.convertBytesToHex(msg)
+      eidReport = eidReport + eid.toString + " "
+    }
+
+    print(eidReport)
+    print(raReport)
+
+    // Need to add tail hex as newline character is causing issues
+    print("2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d")
+  }
+
+  def grpcFinishAttestation(keys: String, eids: String): Unit = {
+    val sc = SparkSession.active.sparkContext
+    if (!sc.isLocal) {
+      numExecutors = sc.getConf.getInt("spark.executor.instances", -1)
+    }
+
+    val rdd = sc.parallelize(Seq.fill(numExecutors) {()}, numExecutors)
+
+    val eidArray = eids.split(" ").map{case eid => eid.toLong}
+    val keyArray = keys.split(" ").map{case hexString => DatatypeConverter.parseHexBinary(hexString)}
+
+    // Runs on executors    
+    val numAttested = Utils.numAttested
+    val eidToKeyMap = (eidArray zip keyArray).toMap
+
+    // TODO: Fix to make it so that Utils.finishAttestation actually does something for remote client.
+    // Currently, since numAttested is full, nothing happens
+    val attestationResults = rdd.mapPartitions { (_) =>
+      val (enclave, eid) = Utils.finishAttestation(numAttested, eidToKeyMap)
+      Iterator((eid, true))
+    }.collect.toMap
+
+    // -1 for failure, 0 for success
+    for ((_, ret) <- attestationResults) {
+      if (!ret)
+        throw new OpaqueException("Attestation failed")
+    }
+  }
 }
