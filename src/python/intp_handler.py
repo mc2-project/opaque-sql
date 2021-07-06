a676b91146d283961b1e6263de2b4c7b4099b44f
Octavian Sima <octavian.sima98@gmail.com>
2021-07-06 21:14:34 +0000

    successfully passing jar to the new python context

--- src/python/intp_handler.py
+++ src/python/intp_handler.py
@@ -1,5 +1,26 @@
+
+from pyspark import SparkConf, SparkContext
+
 from code import InteractiveInterpreter
+
+# This function is run in the programatic interpreter to pass the jars from one Spark context
+# to another
+def set_jars(jars):
+    jars_str = jars.split(",")
+    for jar in jars_str:
+        sc._jvm.addJar(jar)
 
 intp = InteractiveInterpreter(locals=locals())
 intp.runcode("import pyspark.shell")
-intp.runcode("print(\"hello world!\")")
+
+# We need to include the jars provided to Spark in the new IMain's classpath.
+sc = SparkContext.getOrCreate()
+jars = sc.getConf().get("spark.jars", None)
+if not jars:
+  raise Exception("Need to add an assembly jar to spark-submit")
+
+intp.runcode("from intp_handler import set_jars")
+intp.runcode("set_jars(" + jars + ")")
+
+intp.runcode("from opaque_sql import *")
+intp.runcode("init_opaque_sql(testing=True)")
