342966ea060d5f82ed6a5e050b0b53607f01efb7
Octavian Sima <octavian.sima98@gmail.com>
2021-08-10 20:19:06 +0000

    use exec instead of intp

--- src/python/intp_handler.py
+++ src/python/intp_handler.py
@@ -5,17 +5,17 @@ from contextlib import redirect_stdout, redirect_stderr
 
 from pyspark import SparkConf, SparkContext
 
+from opaque_sql import *
+
 class IntpHandler:
     def __init__(self):
-        self.intp = InteractiveInterpreter(locals=locals())
-        self.intp.runcode("from intp_init import intp_init")
-        self.intp.runcode("intp_init()")
+        init_opaque_sql()
 
     def run(self, source):
         out = io.StringIO()
         err = io.StringIO()
         with redirect_stdout(out) and redirect_stderr(err):
-            compiled = code.compile(source)
-            self.intp.runcode(compiled)
+            compiled = compile(source)
+            exec(compiled)
         out.getvalue(), err.getvalue()
 
