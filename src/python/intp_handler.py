cb3ae82f7ddf70f1500effecf7bac17ccbb4eaca
octaviansima <34696537+octaviansima@users.noreply.github.com>
2021-09-22 14:11:38 -0700

    PySpark integration with client (#117)
    This PR includes the necessary changes to make our PySpark listener actually work with the full end-to-end workflow.

--- src/python/intp_handler.py
+++ src/python/intp_handler.py
@@ -1,19 +1,24 @@
-import code
-import io
-from code import InteractiveInterpreter
 from contextlib import redirect_stdout, redirect_stderr
+from io import StringIO
+from traceback import print_exc
 
 from opaque_sql import *
 
+from pyspark.shell import *
+
 class IntpHandler:
     def __init__(self):
-        init_opaque_sql()
+        self.initialized = False
 
     def run(self, source):
-        out = io.StringIO()
-        err = io.StringIO()
-        with redirect_stdout(out) and redirect_stderr(err):
-            compiled = compile(source)
-            exec(compiled)
-        out.getvalue(), err.getvalue()
-
+        with StringIO() as out, redirect_stdout(out), \
+                StringIO() as err, redirect_stderr(err):
+            try:
+                if not self.initialized:
+                    exec("init_opaque_sql()")
+                    self.initialized = True
+                exec(source)
+            except Exception as e:
+                print_exc() # This goes to stderr
+            finally:
+                return out.getvalue(), err.getvalue()
